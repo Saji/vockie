eaa504f2bd8fbe167aa71717a59e307a86eaeacd
Saji Raju <raju.saji@gmail.com>
2010-03-29 15:53:22 +0530

    interface, ajax and sessions improvements.

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,12 +1,43 @@
 $(function() {
-  $(".choice").click(function() {
-    var correct = $("#question").text();
+  var xhrsubpath = '/ajx',
+  choicehandler = function() {
+    var correctques = $("#question").text(),
+    anschoice = $(this).text(),
+    correctans = undefined,
+    responsehandler = function(got) {
+      if(got === "unknown user") {
+        $(".unknowndialog").slideDown();
+        return;
+      }
+      if(got === "off sync") {
+        $(".offsyncdialog").slideDown();
+        return;
+      }
+      $("#dialog").replaceWith(got);
+      $("button.error").click(buttonhandler);
+      $(".choice").click(choicehandler);
+    };
     $(".choice").map(function() {
-      if($(this).attr("ques") === correct) {
+      if($(this).attr("ques") === correctques) {
+        correctans = $(this).text();
         $(this).addClass("correct");
       } else {
         $(this).addClass("wrong");
       }
-    })
-  });   
+    });
+    $("#loading").fadeIn();
+    $.post(xhrsubpath,
+           {ques: correctques, iscorrect: anschoice === correctans},
+           responsehandler
+          );
+  },
+  buttonhandler = function() {
+    location.reload(true);
+  };
+  $("a.settings").click(function(e) {
+    e.preventDefault();
+    $("#settings").slideToggle();
+  })
+  $("button.error").click(buttonhandler);
+  $(".choice").click(choicehandler);   
 });
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-import os, json
-from random import randint, shuffle
+import os, string
+from django.utils import simplejson
+from random import randint, shuffle, Random
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
@@ -7,30 +8,97 @@ from google.appengine.api import users
 from google.appengine.ext import db
 
 dicfile = file("barron.json").read()
-dic = json.loads(dicfile)
+dic = simplejson.loads(dicfile)
 templatepath = os.path.join(os.path.dirname(__file__), 'templates/dialog.tmpl')
+maintemplate = 'main.tmpl'
+ajxtemplate = 'dialogwrapper.tmpl'
+scoreincorrect = 10
+scorecorrect = -5
+maxscore = 100
+choices = 6
 
 def cumulate(dic = dic):
     dic[0][1] = 0
-    for i, x in enumerate(dic): 
+    for i, x in enumerate(dic):
         if i != 0:
             dic[i][1] = dic[i][0] + dic[i-1][1]
+
+def getuser(ident):
+    query = MyUser.all()
+    return query.filter('ident =', ident).get()
+def createuser(user):
+    if isinstance(user, str):
+        user = MyUser(ident=randstr(), name=user, dic=dic);
+    else:
+        user = MyUser(ident=user.user_id(), name=user.nickname(), dic=dic)
+    user.put()
+    return user
+def isgooglecookie(handler):
+    return True if len(getcookie(handler)) > 12 else False
+def deletecookie(handler):
+    del(handler.request.headers['Cookie'])
+def getcookie(handler):
+    if handler.request.headers.has_key('Cookie'):
+        return handler.request.headers['Cookie']
+    else:
+        return ''
+def getpostvar(handler, var):
+    if handler.request.postvars.has_key(var):
+        return handler.request.postvars.get(var)
+    else: return ''
+def cookieuser(handler):
+    cookie = getcookie(handler)
+    return None if not cookie else getuser(cookie)
+def googleuser():
+    return users.get_current_user()
+def randstr():
+    return ''.join(Random().sample(string.letters+string.digits, 12))
+def serve(handler, currentuser):
+    handler.response.headers['Set-Cookie'] = currentuser.ident
+    a = [currentuser.getrandom() for x in range(choices - 1)]
+    a.insert(0, currentuser.getranked())
+    templatedata = {'question': a[0][2],
+              'choices': [{'word': x[2], 'defn': x[3]} for x in a]}
+    if handler.request.is_xhr:
+        templatedata['mode'] = ajxtemplate
+    else:
+        templatedata['mode'] = maintemplate
+        if googleuser():
+            templatedata['loginurl'] = users.create_logout_url(handler.request.path)
+            templatedata['logoption'] = 'Logout'
+        else:
+            templatedata['loginurl'] = users.create_login_url(handler.request.path)
+            templatedata['logoption'] = 'Login'
+    templatedata['name'] = currentuser.name
+    shuffle(templatedata['choices'])
+    handler.response.out.write(template.render(templatepath, templatedata))
+def deletestraycookie(handler):
+    if not googleuser() and isgooglecookie(handler):
+        deletecookie(handler)
 cumulate()
 
-class UserRecord(db.Model):
-    ID = db.StringProperty()
-    name = db.StringProperty()
-    dic = db.StringProperty()
-    settings = db.StringProperty()
-    def test(record, bar):
-        return bar
+################# REWRITE THIS ##########################
+class JSONProperty(db.Property):
+    # data_type = db.Text
+    # def __init__(self, *args, **kwds):
+    #     self._require_parameter(kwds, 'indexed', False)
+    #     kwds['indexed'] = True
+    #     super(JSONProperty, self).__init__(*args, **kwds)
+    def get_value_for_datastore(self, model_instance):
+        value = super(JSONProperty, self).get_value_for_datastore(
+            model_instance)
+        if value is not None:
+            value = simplejson.dumps(value)
+            return db.Text(value)
+    def make_value_from_datastore(self, value):
+        if value is not None:
+            return simplejson.loads(value)
 
-class MyUser:
-    def __init__(self, ID, name, dic, settings):
-        self.id = ID
-        self.name = name
-        self.dic = dic
-        self.settings = settings
+class MyUser(db.Model):
+    ident = db.StringProperty()
+    name = db.StringProperty()
+    dic = JSONProperty()
+    lastattempt = JSONProperty()
     def getranked(self):
         n = randint(0, self.dic[-1][1])
         def closer(a, b, n):
@@ -45,46 +113,63 @@ class MyUser:
                 if closer(this, next, n):
                     ret = dic[i]
                     break
+        self.lastattempt = ret
+        self.put()
         return ret
     def getrandom(self):
         return self.dic[randint(0, len(dic)-1)]
-    def incscore(self, word):
+    def updatescore(self, word, score):
         for d in self.dic:
             if d[2] == word:
-                d[1] += 1
+                if (score < 0 and d[1] < score) or (score + d[1] > maxscore):
+                    return
+                d[1] += score
                 cumulate(self.dic)
+                self.put()
                 break
 
 class MainPage(webapp.RequestHandler):
     def get(self):
-        cookie = self.request.headers['Cookie'] if self.request.headers.has_key('Cookie') else False
-        user_logged = bool(users.get_current_user())
-        known = True if cookie and user_logged else False
-        currentuser = MyUser(randint(0,10000000), 'anon', dic, {})
         self.response.headers['Content-Type'] = 'text/html'
-        if not known:
-            self.response.headers['Set-Cookie'] = currentuser.id
-            a = [currentuser.getranked() for x in range(5)]
-            dialog = {}
-            dialog['question'] = a[0][2]
-            dialog['choices'] = [{'word': x[2], 'defn': x[3]} for x in a]
-            shuffle(dialog['choices'])
-            testuser = UserRecord()
-#            self.response.out.write(testuser.test('foo'))
-            self.response.out.write(template.render(templatepath, dialog))
+        deletestraycookie(self)
+        user = None
+        if googleuser():
+            if not getuser(googleuser().user_id()):
+                user = createuser(user=googleuser())
+            else:
+                user = getuser(googleuser().user_id())
+        elif cookieuser(self):
+            user = cookieuser(self)
         else:
-            pass
+            user = createuser(user='anon')
+        serve(self, user)
 
-
-        # elif user_logged:
-        #     pass
-        # elif cookie:
-        #     self.response.out.write(cookie)
-        # if self.request.is_xhr:
-        #     self.response.out.write('LOL AJAX FUCK YEAH!')
+class AJX(webapp.RequestHandler):
+    def post(self):
+        deletestraycookie(self)
+        user = None
+        question = getpostvar(self, 'ques')
+        iscorrect = getpostvar(self, 'iscorrect')
+        if googleuser():
+            user = getuser(googleuser().user_id())
+        elif cookieuser(self):
+            user = cookieuser(self)
+        else:
+            self.response.out.write('unknown user')
+            return
+        if not question == user.lastattempt[2]:
+            self.response.out.write('off sync')
+            return
+        else:
+            if iscorrect == 'true':
+                user.updatescore(question, scorecorrect)
+            else:
+                user.updatescore(question, scoreincorrect)
+        serve(self, user)                                  
+#        self.response.out.write(user.lastattempt)
 
 def main():
-    application = webapp.WSGIApplication( [('/', MainPage)],
+    application = webapp.WSGIApplication( [('/', MainPage), ('/ajx', AJX)],
                                           debug=True )
     run_wsgi_app(application)
 
